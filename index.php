27b1b95e0d9ba0d783239d53efb25d32b511e6aa
Mahesafy <55169719+mahesafy@users.noreply.github.com>
2023-08-06 21:20:05 +0700

    Menambah Index untuk halaman landing

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Home - Brand</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800&amp;display=swap">
+</head>
+
+<body>
+    <nav class="navbar navbar-light navbar-expand-md fixed-top navbar-shrink py-3" id="mainNav">
+        <div class="container"><a class="navbar-brand d-flex align-items-center" href="/"><span>CSA PROJECT</span></a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navcol-1">
+                <ul class="navbar-nav mx-auto">
+                    <li class="nav-item"><a class="nav-link active" href="index.html">Home</a></li>
+                    <li class="nav-item"><a class="nav-link " href="login-siswa.php">Siswa</a></li>
+                    <li class="nav-item"><a class="nav-link " href="login-admin.php">Admin</a></li>
+                    
+                </ul><a class="btn btn-lg btn-primary shadow" role="button" href="login-siswa.php">START</a>
+            </div>
+        </div>
+    </nav>
+    <header class="pt-5">
+        <div class="container pt-4 pt-xl-5" style="margin-bottom: 51px;padding-top: 66px;margin-top: -86px;">
+            <div class="row pt-5" style="margin-bottom: 94px;margin-right: -12px;padding-bottom: 64px;padding-top: 0px;margin-top: 126px;">
+                <div class="col-md-8 text-center text-md-start mx-auto">
+                    <div class="text-center">
+                        <h1 class="display-4 fw-bold mb-5">Aplikasi Pembayaran SPP Sekolah</h1>
+                        <p class="fs-5 text-muted mb-5">By Mahesa Ramadan &amp; Teguh Bagas</p>
+                        <form class="d-flex justify-content-center flex-wrap" method="post">
+                            <div class="shadow-lg mb-3"></div>
+                            <div class="shadow-lg mb-3"></div>
+                        </form>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-10 mx-auto">
+                    <div class="text-center position-relative"></div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <footer>
+        <div class="container py-4 py-lg-5">
+            <div class="text-muted d-flex justify-content-between align-items-center pt-3">
+                <p class="mb-0">Copyright © 2023 CSA</p>
+                <ul class="list-inline mb-0">
+                    <li class="list-inline-item"><a href="https://github.com/mahesafy/SPP-WEB-CSA"><small class="form-text">GITHUB</small></a></li>
+                    <li class="list-inline-item"></li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/bold-and-bright.js"></script>
+</body>
+
+</html>
